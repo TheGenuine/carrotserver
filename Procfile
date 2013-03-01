1e14bfaeae2cecead3f103fa178b47d1111bc61e
Rene Ruck <reneruck@gmail.com>
2013-03-01 19:08:51 +0000

    initial commit basic impl and heroku deployable

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node carrotCloudService.js
---
+++ carrotCloudService.js
@@ -0,0 +1,7 @@
+var net = require('net');
+var s = new net.Server();
+s.on('connection', function(socket) { 
+	socket.write("I'm here");
+	console.log("Connection!"); 
+});
+s.listen(42424);
