29c33194e949d8746b2010a5415accb2c93e3a96
Rene Ruck <reneruck@gmail.com>
2013-03-02 00:49:18 +0000

    add persisting of incoming data

--- dataHandler.js
+++ dataHandler.js
@@ -1,4 +1,14 @@
 exports.handle = function(data) {
-	console.log("Received following data" + data);
+	console.log("Received data" + data);
+	var carrotData = JSON.parse(data);
+
+	var db = require('nano')('http://localhost:5984/carrot');
+	
+	console.log("Persisting data");
+	db.insert(carrotData, '',  function(err, body) {
+	  if (!err)
+	  	console.log(body);
+	});
+
 	return true;
 }
