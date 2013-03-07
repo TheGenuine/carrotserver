4f0c5ef33d97c612edcb27ecbbe7b34e3b2c57a8
Rene Ruck <reneruck@gmail.com>
2013-03-07 17:52:47 +0000

    use express for static page delivery

--- carrotCloudService.js
+++ carrotCloudService.js
@@ -1,38 +1,51 @@
 // Load the http module to create an http server.
-var http = require('http');
 var net = require('net');
+var express = require('express');
+
+/**
+ * Web server
+ */
+var app = express();
+app.use(express.logger());
+app.use(express.static(__dirname + '/www'));
+
+app.post('/', function(req, resp, next){
+	resp.sendfile(__dirname + '/www/home.html');
+});
+
+app.listen(80);
+console.log("Web Server listening on Port 80")
+
+/**
+ * Low level data server
+ */
 var s = new net.Server();
+var socket_global;
+
 var dataListener = function(data) {
 	console.log("Received Data");
 	console.log("Handling input ...");
-	var response = require('./dataHandler').handle(data);
-	console.log("result was " + response);
+	var result = require('./dataHandler').handle(data);
+	if(result && socket_global != null) {
+		socket_global.send(200);
+	} else {
+		socket_global.send(500);
+	}
 };
 
 s.on('connection', function(socket) { 
   console.log("Connection!"); 
   socket.setEncoding("UTF-8");
+  socket_global = socket;
   socket.on('data', dataListener);
 });
 
 s.on('close', function(had_error) {
+	socket_global = null;
 	console.log("Disconnected");
 });
-console.log("Server listening");
 s.listen(42424);
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/html"});
-
-  response.end("<html><h1><code>Welcome to Node.js on CloudBees</code></h1>\
-    <code>Made with a <a href='https://github.com/CloudBees-community/nodejs-clickstart'>ClickStart</a></code><p>\
-    <code>How it <a href='https://github.com/CloudBees-community/node-clickstack'>works</a> (a ClickStack)\
-     and <a href='http://developer.CloudBees.com'>docs</a></code><p><p></html>");
-});
-
-
-server.listen(80);
+console.log("Data Server listening on Port 42424");
 
 // Put a friendly message on the terminal
 console.log("Node Server running");
